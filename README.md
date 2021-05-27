a58b094f91f331a559aa4c5803400f161792cd85
Jarid Poe <jaridpoe@gmail.com>
2021-05-27 19:46:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# selbina
+
+
+How to run:
+
+1. npm run start
+2. Go to http://localhost:3000/
---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path'
+
+const app = express();
+const __dirname = path.resolve()
+
+// app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
