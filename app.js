ec0ad25bae75fed6659fa0a44219bcf285d9f1b2
Jarid Poe <jaridpoe@gmail.com>
2021-05-27 21:35:59 -0400

    fixed static assets

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from 'path'
 const app = express();
 const __dirname = path.resolve()
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
