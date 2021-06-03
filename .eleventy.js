00142ff19f00327656e65d528fce507bfdbece72
Jarid Poe <jaridpoe@gmail.com>
2021-06-02 22:07:16 -0400

    11ty

---
+++ .eleventy.js
@@ -0,0 +1,48 @@
+const pluginTailwindCSS = require("eleventy-plugin-tailwindcss")
+const navigation = require("@11ty/eleventy-navigation")
+const seo = require("eleventy-plugin-seo")
+const unpkgInliner = require("eleventy-njk-unpkg-inliner")
+const site = require("./src/_data/site.js")
+
+module.exports = function(eleventyConfig) {
+
+    // TEMPLATES
+    eleventyConfig.addLayoutAlias('default', 'layouts/base.njk')
+    
+    // PLUGINS
+    eleventyConfig.addPlugin(pluginTailwindCSS, {
+        src: "src/css/style.css"
+    })
+    
+    eleventyConfig.addPlugin(navigation);
+
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    eleventyConfig.addPlugin(seo, {
+        title: site.title,
+        description: site.description,
+        url: site.url,
+        author: site.author.name,
+        twitter: site.author.twitter,
+        image: site.image
+      })
+    
+    // SHORTCODES
+    eleventyConfig.addNunjucksAsyncShortcode('inline', unpkgInliner)
+
+    // DEEP DATA MERGE
+    eleventyConfig.setDataDeepMerge(true)
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist'
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        passthroughFileCopy: true
+
+    }
+
+}
---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "title" : "Selbina",
+    "description" : "Selbina",
+    "author" : {
+        "name": "Jarid Poe",
+        "github" : "jaridpoe",
+        "twitter": "jaridpoe",
+        "avatar": "https://secure.gravatar.com/avatar"
+        },
+    "buildTime": new Date()
+}
